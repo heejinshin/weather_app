28bad38963e506c6716bd8f3e16cc643f447a337
skyni25 <qkrrlxo92@koreatech.ac.kr>
2023-12-07 20:40:57 +0900

    파파코 API 사용하여 지역이름 영어로 나오는거 한글로 나오도록 함

--- src/main/java/com/example/weather_app/Weather/Controller/WeatherController.java
+++ src/main/java/com/example/weather_app/Weather/Controller/WeatherController.java
@@ -6,12 +6,14 @@ import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.math.BigDecimal;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class WeatherController {
@@ -21,6 +23,12 @@ public class WeatherController {
 
     @Value("${weather-API-Key}")
     private String serviceKey;
+
+    @Value("${papago-client-ID}")
+    private String clientId;
+
+    @Value("${papago-client-SECRET}")
+    private String clientSecret;
 
     @RequestMapping(value = "/curr-weather")
     @CrossOrigin(origins = "*")
@@ -65,12 +73,27 @@ public class WeatherController {
         JSONObject windObj = (JSONObject) jo.get("wind");
         JSONObject response = new JSONObject();
 
-        String name = (String) jo.get("name");
+        String description = (String) weaObj.get("description");
         Integer rowId = (Integer) weaObj.get("id");
         double id = rowId.doubleValue();
         id = Math.floor(id * 0.01) * 100; // 100 자리로 보여주기 위해 컨버팅 처리
 
-        String description = (String) weaObj.get("description");
+        String regionName = (String) jo.get("name");
+
+        // 지역명 영어에서 한글로 번역
+        try {
+            regionName = URLEncoder.encode(regionName, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException("인코딩 실패", e);
+        }
+
+        Map<String, String> requestHeaders = new HashMap<>();
+        requestHeaders.put("X-Naver-Client-Id", clientId);
+        requestHeaders.put("X-Naver-Client-Secret", clientSecret);
+
+        JSONObject papagoResult = new JSONObject(post("https://openapi.naver.com/v1/papago/n2mt", requestHeaders, regionName));
+        String name = (String)((JSONObject)((JSONObject) papagoResult.get("message")).get("result")).get("translatedText");
+
         BigDecimal temp = (BigDecimal) mainObj.get("temp");
         BigDecimal feels_like = (BigDecimal) mainObj.get("feels_like");
         BigDecimal temp_min = (BigDecimal) mainObj.get("temp_min");
@@ -100,5 +123,61 @@ public class WeatherController {
         response.put("snow_1h", snow_1h);
 
         return response.toString();
+    }
+
+    private String post(String apiUrl, Map<String, String> requestHeaders, String text){
+        HttpURLConnection con = connect(apiUrl);
+        String postParams = "source=en&target=ko&text=" + text; //원본언어: 한국어 (ko) -> 목적언어: 영어 (en)
+        try {
+            con.setRequestMethod("POST");
+            for(Map.Entry<String, String> header :requestHeaders.entrySet()) {
+                con.setRequestProperty(header.getKey(), header.getValue());
+            }
+
+            con.setDoOutput(true);
+            try (DataOutputStream wr = new DataOutputStream(con.getOutputStream())) {
+                wr.write(postParams.getBytes());
+                wr.flush();
+            }
+
+            int responseCode = con.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) { // 정상 응답
+                return readBody(con.getInputStream());
+            } else {  // 에러 응답
+                return readBody(con.getErrorStream());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("API 요청과 응답 실패", e);
+        } finally {
+            con.disconnect();
+        }
+    }
+
+    private HttpURLConnection connect(String apiUrl){
+        try {
+            URL url = new URL(apiUrl);
+            return (HttpURLConnection)url.openConnection();
+        } catch (MalformedURLException e) {
+            throw new RuntimeException("API URL이 잘못되었습니다. : " + apiUrl, e);
+        } catch (IOException e) {
+            throw new RuntimeException("연결이 실패했습니다. : " + apiUrl, e);
+        }
+    }
+
+    private String readBody(InputStream body){
+        InputStreamReader streamReader = new InputStreamReader(body);
+
+        try (BufferedReader lineReader = new BufferedReader(streamReader)) {
+            StringBuilder responseBody = new StringBuilder();
+
+            String line;
+            while ((line = lineReader.readLine()) != null) {
+                responseBody.append(line);
+            }
+
+            return responseBody.toString();
+        } catch (IOException e) {
+            throw new RuntimeException("API 응답을 읽는데 실패했습니다.", e);
+        }
     }
 }
